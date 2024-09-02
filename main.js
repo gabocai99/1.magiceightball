681031312dac926fc0a2bd94ffac86783b15dbae
Popescu Gabriel <89848757+gabocai99@users.noreply.github.com>
2024-09-02 15:43:00 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+var userName = '';
+userName === '' ? console.log('Hello!') : console.log('Hello, ' + userName + '!');
+
+var userQuestion = 'How are you doing?'
+console.log('How are you doing ' + userName +'?');
+
+var randomNumber = Math.floor(Math.random() * 8);
+
+var eightBall = '';
+eightBall = randomNumber;
+
+if (eightBall === 1){
+  console.log(`The ball is ${eightBall}`);
+} else if(eightBall === 2){
+  console.log(`The ball is ${eightBall}`);
+} else if(eightBall === 3){
+  console.log(`The ball is ${eightBall}`);
+} else if(eightBall === 4){
+  console.log(`The ball is ${eightBall}`);
+  } else if(eightBall === 5){
+  console.log(`The ball is ${eightBall}`);
+  } else if(eightBall === 6){
+  console.log(`The ball is ${eightBall}`);
+  } else if(eightBall === 7){
+  console.log(`The ball is ${eightBall}`);
+  } else if(eightBall === 8){
+  console.log(`The ball is ${eightBall}`);
+  } else {
+    console.log('XXXXXXXXXXX');
+  }
